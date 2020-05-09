24d180804298264b18072ec86e90fc0c99a1fbbd
Grzegorz Wawrzak <grzegorz.wawrzak@softnauts.com>
2020-05-09 16:52:21 +0900

    Fix errors

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,9 @@
 import {Module} from '@nestjs/common';
-import {HealthController} from './health.controller';
+import {HealthController} from './controller/health.controller';
 import {TerminusModule} from '@nestjs/terminus';
 import {DatabaseOrmModule} from '../database-orm.module';
 import {ClientsModule} from '@nestjs/microservices';
-import {HealthCheckController} from './health-check.controller';
+import {HealthCheckController} from './controller/health-check.controller';
 import {grpcClientOptions} from '../grpc-client.options';
 
 @Module({
